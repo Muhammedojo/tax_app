862859c0aa5a4ef07bb51482d0020d7e3c0e53bb
Muhammed <ojomuhammad@gmail.com>
2026-03-19 18:02:12 +0100

    Added the model classes and endpoints

--- lib/core/api/services/endpoints.dart
+++ lib/core/api/services/endpoints.dart
@@ -2,25 +2,19 @@ String get baseUrl => "http://206.189.120.212";
 
 String get baseApi => "$baseUrl/api/v1/";
 
-const languageEndpoint = "meta/languages"; 
+const languageEndpoint = "meta/languages";
 const roleEndpoint = "meta/roles";
 const topicEndpoint = "meta/topics";
 
 const lawRegistryEndpoint = "laws/registry";
 const lawEndpoint = "laws";
-const lawSectionEndpoint = "laws/${'sectionId'}";
+const lawSectionEndpoint = "laws/${2}";
 
 const aiQueryEndpoint = "ai/query";
 
 const offlinePackEndpoint = "offline-pack";
 
 const updateEndpoint = "updates";
-const updateFilterEndpoint = "updates/${'updateId'}";
+const updateFilterEndpoint = "updates/${1}";
 
-const searchEndpoint = "search?q=${'query'}&law_id=${'lawId'}";
-
-
-
-
-
-
+const searchEndpoint = "search?q=${'query'}&law_id=${1}";
